b00b2ff93885fb394b54487ca33bfd7fb4240b67
Adam Kadwory <adamkadwory@gmail.com>
2024-11-28 00:26:55 -0800

    project structure (logging and exceptions)

---
+++ src/logger.py
@@ -0,0 +1,19 @@
+import logging 
+import os 
+from datetime import datetime
+
+#Define the log file file and name 
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_dir = os.path.join(os.getcwd(),"logs") #Directory
+os.makedirs(log_dir,exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(log_dir,LOG_FILE) #Full log file path 
+
+#Configure logging 
+logging.basicConfig(
+    filename= LOG_FILE_PATH,
+    format= "[%(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level = logging.INFO,
+
+
+)
