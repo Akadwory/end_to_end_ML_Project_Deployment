f1de0873260bae7a7ef5d491a4213304fcde6032
Adam Kadwory <adamkadwory@gmail.com>
2024-12-13 19:01:06 -0800

    model Preprocessing, transformation and save processor as a pickle file in artifacts dir

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,16 +3,16 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd 
-
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformation
+
 
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifacts',"train.csv")
     test_data_path: str = os.path.join('artifacts',"test.csv")
     raw_data_path: str=os.path.join('artifacts',"data.csv")
-    valid_data: str=os.path.join('artifacts',"valid.csv")
 
 class DataIngestion:
     def __init__(self):
@@ -32,7 +32,6 @@ class DataIngestion:
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-
             logging.info("Ingestion of the data is completed")
 
             return (
@@ -46,6 +45,9 @@ class DataIngestion:
 
 if __name__=="__main__":
     obj =DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
 
